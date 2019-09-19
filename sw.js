6e70ddfc651ac7ccd4c8eb95aa90948cb70ad5ee
muttons <55284268+muttons@users.noreply.github.com>
2019-09-19 09:55:50 -0400

    updating to test new service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
- 
+ /* working install code commented out for testing of new code
 self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('video-store').then(function(cache) {
@@ -24,5 +24,47 @@ self.addEventListener('fetch', function(e) {
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
     })
+  );
+});
+
+*/
+
+/* test code */
+var cacheName = 'thirdVersion';
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+      .then(cache => cache.addAll([
+        '/test/dog.jpg',
+      '/test/',
+       '/test/index.html',
+       '/test/index.js',
+       '/test/style.css',
+       '/test/company-directory.html',
+       '/test/company-directory-it.html',
+       '/test/information-technology.html',
+       '/test/helpdesk-request.html',
+       '/test/images/flower-512.png',
+       '/test/images/flower-192.png'
+      ]))
+  );
+});
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
   );
 });
