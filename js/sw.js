1f2e2c1cd58ffcd25fe47187776e42a04b37b6e1
muttons <55284268+muttons@users.noreply.github.com>
2020-02-03 11:20:08 -0500

    Add files via upload

---
+++ js/sw.js
@@ -0,0 +1,49 @@
+var CACHE_NAME = 'v1';
+var urlsToCache = [
+  '/schspa/',
+  'index.html',
+  'js/index.js',
+  'css/style.css',
+  'agency_directory.html',
+  'event-calendar.html',
+  'event-monday.html',
+  'event-tuesday.html',
+  'event-wednesday.html',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+    .then( cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+
+        }).map(function(cacheName) {
+          return caches.delete(cacheName)
+        })
+      );
+    })
+  );
+});
+
+
+
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+
+    fetch(event.request).catch(() => {
+
+      return caches.match(event.request);
+    })
+  );
+});
