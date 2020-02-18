d9abc11f29f4dc270fc1875daa62146f62dd4f3a
muttons <55284268+muttons@users.noreply.github.com>
2020-02-18 08:28:47 -0500

    initialcommit

--- README.md
+++
@@ -1,1 +0,0 @@
-This is a test website being built with materializeand firebase before going live.
---
+++ sandbox.js
@@ -0,0 +1,14 @@
+let age = 25;
+let year = 2020;
+
+console.log(age, year);
+
+age = 30;
+console.log('this is a console log as a string');
+
+
+const points = 100;
+console.log(points);
+
+
+//this is a comment 
